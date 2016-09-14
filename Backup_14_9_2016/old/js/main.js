96326bbb55025ed5115cbfd162a3567129db0b3c
Alvaro Uribe <alvaro.uribe@vocus.co.nz>
2016-09-14 15:24:36 +1200

    Added a backup

---
+++ Backup_14_9_2016/old/js/main.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function() {
+  /* Pop Video Box */
+  jQuery('.playVideoButton').fancybox({
+    'padding': 0,
+    'width': '100%',
+    'height': '100%',
+    'type': 'iframe'
+    });
+});
